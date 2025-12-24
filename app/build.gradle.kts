af3d8d3b3f3c589d40b6e10e850dfd1290af077c
MK X Shaon <mkshaonnew31@gmail.com>
2025-12-24 08:16:34 +0600

    Fix build errors and update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,8 +74,6 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:34.7.0"))
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-crashlytics")
-    implementation("com.google.firebase:firebase-auth")
-    implementation("com.google.firebase:firebase-firestore")
     implementation("com.google.firebase:firebase-messaging")
     
     // Google Sign-In
