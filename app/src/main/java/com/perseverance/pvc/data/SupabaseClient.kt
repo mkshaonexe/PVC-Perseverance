df84898b566fd41d1b05b4528fb5d406573ccb83
MK X Shaon <mkshaonnew31@gmail.com>
2025-12-24 20:36:23 +0600

    Add SupabaseClient singleton

---
+++ app/src/main/java/com/perseverance/pvc/data/SupabaseClient.kt
@@ -0,0 +1,18 @@
+package com.perseverance.pvc.data
+
+import com.perseverance.pvc.BuildConfig
+import io.github.jan.supabase.createSupabaseClient
+import io.github.jan.supabase.gotrue.GoTrue
+import io.github.jan.supabase.postgrest.Postgrest
+import io.github.jan.supabase.realtime.Realtime
+
+object SupabaseClient {
+    val client = createSupabaseClient(
+        supabaseUrl = BuildConfig.SUPABASE_URL,
+        supabaseKey = BuildConfig.SUPABASE_ANON_KEY
+    ) {
+        install(GoTrue)
+        install(Postgrest)
+        install(Realtime)
+    }
+}
