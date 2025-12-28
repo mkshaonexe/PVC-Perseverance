871ecfbd136c53e60355cfe0314a9238ffb39ef7
MK X Shaon <mkshaonnew31@gmail.com>
2025-12-28 12:15:42 +0600

    Add Midnight Theme colors

--- app/src/main/java/com/perseverance/pvc/ui/theme/Color.kt
+++ app/src/main/java/com/perseverance/pvc/ui/theme/Color.kt
@@ -24,6 +24,14 @@ val DarkSurfaceVariant = Color(0xFF2C2C2C)
 val LightGrey = Color(0xFFE0E0E0)
 val MediumGrey = Color(0xFFB0B0B0)
 
+// Midnight Theme Colors
+val MidnightBackground = Color(0xFF0D161F)
+val MidnightSurface = Color(0xFF151F28)
+val MidnightSurfaceVariant = Color(0xFF1E2A36)
+val MidnightPrimary = Color(0xFFFFD700) // Gold
+val MidnightOnBackground = Color(0xFFFFFFFF)
+val MidnightSecondary = Color(0xFF8B9BB4) // Muted blue/grey for secondary text
+
 // Legacy colors for compatibility
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
