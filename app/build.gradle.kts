007e65081935caeb6ffefe5971f03e013a425159
MK X Shaon <mkshaonnew31@gmail.com>
2025-12-31 06:33:14 +0600

    Update versionName to 0.7.9 and increment versionCode to 16 in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.perseverance.pvc"
         minSdk = 24
         targetSdk = 36
-        versionCode = 15
-        versionName = "0.7.6"
+        versionCode = 16
+        versionName = "0.7.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
